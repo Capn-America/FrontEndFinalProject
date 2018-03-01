3097e7b3d8453b89ae152f55fefd8f924f0900f8
Stephen Rogers <stephenrogers74@yahoo.com>
2018-03-01 15:23:07 -0600

    Page Layout finished, time for linking.

---
+++ script.js
@@ -0,0 +1,16 @@
+function loadRepo(url, callback) 
+{
+    const gitHubRequest = new XMLHttpRequest();
+      // Finish your request here
+      // Don't forget the callback()
+  }
+  
+  function loadRepoCallback(gitHubRequest) 
+  {
+    // Callback function needs to parse the JSON
+    // Using the DOM get the element ID from your <ul> in your HTML
+    // You will need a forEach to loop through the parsed object
+    // Using the DOM create new list elements
+    // Plug in the .html_url and .name from the parsed object.  To make this a link you'll need 
+    // Append the new list items to the element you retrieved from HTML
+  }
