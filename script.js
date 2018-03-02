3034bcb3af60e38782b4c0b4338f0552e96f791e
Stephen Rogers <stephenrogers74@yahoo.com>
2018-03-01 19:24:56 -0600

    Everything, but the AJAX part.

--- script.js
+++ script.js
@@ -14,3 +14,50 @@ function loadRepo(url, callback)
     // Plug in the .html_url and .name from the parsed object.  To make this a link you'll need 
     // Append the new list items to the element you retrieved from HTML
   }
+
+
+  function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("gitHubReps").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "file:///Users/stephenrogers/Desktop/Woz%20U/Front%20End%20Foundations/FrontEndFinal/FrontEndFinalProject/API.txt", true);
+    xhttp.send();
+  }
+
+
+
+
+
+
+  function turnGrey()
+  {
+    
+    
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+   
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        document.getElementById("original").href="style2.css";
+    } else {
+        document.getElementById("original").href="style.css";
+    }
+  }
+
+
+document.getElementById("theShining").onmouseover = function() {mouseOver()};
+document.getElementById("theShining").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.getElementById("theShining").src="file:///Users/stephenrogers/Desktop/Woz%20U/Front%20End%20Foundations/FrontEndFinal/FrontEndFinalProject/pictures/shining2.png";
+}
+
+function mouseOut() {
+    document.getElementById("theShining").src = "file:///Users/stephenrogers/Desktop/Woz%20U/Front%20End%20Foundations/FrontEndFinal/FrontEndFinalProject/pictures/shining.png";
+}
+
+
